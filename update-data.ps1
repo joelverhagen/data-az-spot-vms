bbd47f677ddeb33d8bf7ea03cb83ab85b34d7918
Joel Verhagen <jver@microsoft.com>
2022-05-21 16:14:43 -0700

    Consider MaxResourceVolumeMB size as well (this can be used for ephemeral OS disks)
    Write out SKU details to a CSV

--- update-data.ps1
+++ update-data.ps1
@@ -1,64 +1,78 @@
-$allowNoOp = $false
 $regionsPath = Join-Path $PSScriptRoot "regions.txt"
 $vmSkusPath = Join-Path $PSScriptRoot "vm-skus.txt"
+$vmSkuInfoPath = Join-Path $PSScriptRoot "vm-skus.csv"
 $spotPricesDir = Join-Path $PSScriptRoot "spot-prices"
 
-if ($allowNoOp -and (Test-Path $regionsPath)) {
-    $regions = Get-Content $regionsPath
-}
-else {
-    Write-Host "Getting regions in US and Canada"
-    $regions = az account list-locations `
-        --query "[?(metadata.geographyGroup == 'US' || metadata.geographyGroup == 'Canada') && not_null(metadata.latitude)].name" `
-        --output tsv | `
-        Where-Object { !$_.EndsWith("euap") } | `
-        Sort-Object
-    $regions | Out-File $regionsPath -Encoding utf8
-}
+Write-Host "Getting regions in US and Canada"
+$regions = az account list-locations `
+    --query "[?(metadata.geographyGroup == 'US' || metadata.geographyGroup == 'Canada') && not_null(metadata.latitude)].name" `
+    --output tsv | `
+    Where-Object { !$_.EndsWith("euap") } | `
+    Sort-Object
+$regions | Out-File $regionsPath -Encoding utf8
 
-if ($allowNoOp -and (Test-Path $vmSkusPath)) {
-    $vmSkus = Get-Content $vmSkusPath
-}
-else {
-    $vmSkus = @()
-    foreach ($region in $regions) {
-        Write-Host "Getting VM SKUs in $region"
-        $regionSkusJson = az vm list-skus `
-            --location $region `
-            --resource-type "virtualMachines" `
-            --size "Standard_"
-        $regionSkus = $regionSkusJson.Replace("""Name""", """_Name""") | ConvertFrom-Json
-        foreach ($sku in $regionSkus) {
-            $capabilities = @{}
-            foreach ($pair in $sku.capabilities) {
-                $capabilities[$pair.name] = $pair.value
+$skuNameToInfo = @{}
+$skuNameToRegions = @{}
+foreach ($region in $regions) {
+    Write-Host "Getting VM SKUs in $region"
+    $regionSkusJson = az vm list-skus `
+        --location $region `
+        --resource-type "virtualMachines" `
+        --size "Standard_"
+    $regionSkus = $regionSkusJson.Replace("""Name""", """_Name""") | ConvertFrom-Json
+    foreach ($sku in $regionSkus) {
+        $capabilities = @{}
+        foreach ($pair in $sku.capabilities) {
+            $capabilities[$pair.name] = $pair.value
+        }
+        
+        $skuInfo = [PSCustomObject]@{
+            Name                      = $sku.name;
+            vCPUs                     = [int]$capabilities.vCPUs;
+            MemoryGB                  = [int]$capabilities.MemoryGB;
+            CachedDiskBytes           = [long]$capabilities.CachedDiskBytes;
+            MaxResourceVolumeMB       = [long]$capabilities.MaxResourceVolumeMB;
+            CpuArchitectureType       = $capabilities.CpuArchitectureType;
+            VMDeploymentTypes         = $capabilities.VMDeploymentTypes;
+            LowPriorityCapable        = [Convert]::ToBoolean($capabilities.LowPriorityCapable);
+            EphemeralOSDiskSupported  = [Convert]::ToBoolean($capabilities.EphemeralOSDiskSupported);
+            EncryptionAtHostSupported = [Convert]::ToBoolean($capabilities.EncryptionAtHostSupported);
+        }
+
+        $minDiskSizeMB = 30 * 1024 # [smalldisk] Windows images require 30 GB
+        $minDiskSizeBytes = $minDiskSizeMB * 1024 * 1024
+
+        $candidate = $skuInfo.vCPUs -ge 2 -and $skuInfo.vCPUs -le 16 -and `
+            $skuInfo.MemoryGB -ge 4 -and $skuInfo.MemoryGB -le 32 -and `
+        ($skuInfo.CachedDiskBytes -ge $minDiskSizeBytes -or $skuInfo.MaxResourceVolumeMB -ge $minDiskSizeMB) -and `
+            $skuInfo.CpuArchitectureType -eq "x64" -and `
+            $skuInfo.VMDeploymentTypes.Contains("IaaS") -and `
+            $skuInfo.LowPriorityCapable -and `
+            $skuInfo.EphemeralOSDiskSupported -and `
+            $skuInfo.EncryptionAtHostSupported;
+
+        if ($candidate) {
+            if (!$skuNameToInfo[$skuInfo.Name]) {
+                $skuNameToInfo[$skuInfo.Name] = $skuInfo
+                $skuNameToRegions[$skuInfo.Name] = @($region)
             }
-
-            $vCPUs = [int]$capabilities.vCPUs
-            $memoryGB = [int]$capabilities.MemoryGB
-            $cachedDiskBytes = [long]$capabilities.CachedDiskBytes
-            $minCachedDiskBytes = 30 * 1024 * 1024 * 1024
-            $cpuArchitectureType = $capabilities.CpuArchitectureType
-            $vmDeploymentTypes = $capabilities.VMDeploymentTypes
-            $lowPriorityCapable = [Convert]::ToBoolean($capabilities.LowPriorityCapable)
-            $ephemeralOSDiskSupported = [Convert]::ToBoolean($capabilities.EphemeralOSDiskSupported)
-            $encryptionAtHostSupported = [Convert]::ToBoolean($capabilities.EncryptionAtHostSupported)
-
-            if ($vCPUs -ge 2 -and $vCPUs -le 16 -and `
-                    $memoryGB -ge 4 -and $memoryGB -le 32 -and `
-                    $cachedDiskBytes -ge $minCachedDiskBytes -and `
-                    $cpuArchitectureType -eq "x64" -and `
-                    $vmDeploymentTypes.Contains("IaaS") -and `
-                    $lowPriorityCapable -and `
-                    $ephemeralOSDiskSupported -and `
-                    $encryptionAtHostSupported) {
-                $vmSkus += @($sku.name)
+            else {
+                if (Compare-Object $skuNameToInfo[$skuInfo.Name].PSObject.Properties $skuInfo.PSObject.Properties) {
+                    throw "Found inconsistent SKU info. Region $region is different than $($skuNameToRegions[$skuInfo.Name] -join ', ')."
+                }
+                $skuNameToRegions[$skuInfo.Name] = ($skuNameToRegions[$skuInfo.Name] + @($region)) | Sort-Object
             }
         }
     }
-    $vmSkus = $vmSkus | Sort-Object | Select-Object -Unique
-    $vmSkus | Out-File $vmSkusPath -Encoding utf8
 }
+
+$vmSkus = $skuNameToInfo.Values | `
+    Sort-Object -Property Name | `
+    ForEach-Object { $_ | Add-Member -MemberType NoteProperty -Name 'Regions' -Value ($skuNameToRegions[$_.Name] -join ","); $_ }
+$vmSkus | Export-Csv -Path $vmSkuInfoPath -Encoding utf8 -NoTypeInformation
+
+$vmSkuNames = $vmSkus | ForEach-Object { $_.Name }
+$vmSkuNames | Out-File -FilePath $vmSkusPath -Encoding utf8
 
 # Azure Retail Prices API
 # Docs: https://docs.microsoft.com/en-us/rest/api/cost-management/retail-prices/azure-retail-prices
@@ -67,7 +81,7 @@ if (!(Test-Path $spotPricesDir)) {
     New-Item $spotPricesDir -ItemType Directory | Out-Null
 }
 
-foreach ($vmSku in $vmSkus) {
+foreach ($vmSku in $vmSkuNames) {
     $baseUrl = "https://prices.azure.com/api/retail/prices"
     $apiVersion = "2021-10-01-preview"
     $currencyCode = "USD"
@@ -88,7 +102,7 @@ foreach ($vmSku in $vmSkus) {
     $url = "$($baseUrl)?api-version=$apiVersion&currencyCode=$currencyCode&meterRegion=$meterRegion&`$filter=$($criteria -join " and ")"
 
     Write-Host "Getting prices for $vmSku"
-    $response = Invoke-WebRequest $url -UserAgent "GitHub Actions - joelverhagen/data-azure-spot-vms"
+    $response = Invoke-WebRequest $url -UserAgent "GitHub Actions - joelverhagen/data-azure-spot-vms" -UseBasicParsing
     $response.Content | `
         jq '.Items[] | { armRegionName: .armRegionName, retailPrice: .retailPrice, meterId: .meterId }' | `
         jq --slurp '. | sort_by(.armRegionName, .meterId)[]' | `
